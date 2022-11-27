efcfc069411eb4c5c352967e0064cdfab774c54b
Uğur Kozgül <ugurkozgul@gmail.com>
2022-11-28 01:18:16 +0300

    Movie sınıfı oluşturuldu

---
+++ Movie.cs
@@ -0,0 +1,8 @@
+public class Movies
+{
+    public int id { get; set; }
+    public string title { get; set; }
+    public int raiting { get; set; }
+    public string genre { get; set; }
+    public int duration { get; set; }
+}
