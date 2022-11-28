d39b0a289aa2bf511aeae277d076cd74892c0328
ugur <ugurkozgul@gmail.com>
2022-11-28 14:07:09 +0300

    html algoritma yapıldı ve sayfa oluşturldu

--- Program.cs
+++ Program.cs
@@ -9,16 +9,30 @@ class ReadJsonFile
         var movie = JsonSerializer.Deserialize<List<Movies>>(jsonString);
 
         //Tablo başla
-        string tabloBasla = "";
+        string tabloBasla = "<table>";
         //Tablo Basliklar
-        string tabloBasliklar = "";
+        string tabloBasliklar = "<th> <tr> <td>id</td> <td>title</td> <td>rating</td> <td>genre</td> <td>duration</td> </tr> </th>";
         //Tablo gövde 
         string tabloGovde = "";
+        tabloGovde += "<tbody>";
+        foreach (var item in movie)
+        {
+            tabloGovde += "<tr>" +
+            $"<td>{item.id}</td>" +
+            $"<td>{item.title}</td>"+
+            $"<td>{item.raiting}</ td >" +
+            $"< td >{item.genre}</ td >" +
+            $"< td > {item.duration} </ td >" +
+        "</ tr >";
+        }
+        tabloGovde += "</tbody>";
+
         //tablo kapat
-        string tabloKapat = "";
+        string tabloKapat = "</table>";
+
 
         string tablo = tabloBasla + tabloBasliklar + tabloGovde + tabloKapat;
-        
+        File.WriteAllText("output.html", tablo);
 
     }
 }
