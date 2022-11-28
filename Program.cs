48835795057df36815af3baaf9ba9f73a153bdeb
ugur <ugurkozgul@gmail.com>
2022-11-28 13:43:56 +0300

    html algoritma tamamlandı

--- Program.cs
+++ Program.cs
@@ -7,6 +7,18 @@ class ReadJsonFile
         string fileName = @"./movies.json";
         string jsonString = File.ReadAllText(fileName);
         var movie = JsonSerializer.Deserialize<List<Movies>>(jsonString);
+
+        //Tablo başla
+        string tabloBasla = "";
+        //Tablo Basliklar
+        string tabloBasliklar = "";
+        //Tablo gövde 
+        string tabloGovde = "";
+        //tablo kapat
+        string tabloKapat = "";
+
+        string tablo = tabloBasla + tabloBasliklar + tabloGovde + tabloKapat;
         
+
     }
 }
